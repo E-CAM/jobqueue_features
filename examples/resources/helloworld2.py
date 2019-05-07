52322c370691c65b582d31b183b69370056a1ae8
Alan O'Cais <a.ocais@fz-juelich.de>
2019-05-07 16:03:30 +0200

    Ignore missing GPUTILS module if tripped

--- examples/resources/helloworld2.py
+++ examples/resources/helloworld2.py
@@ -15,10 +15,12 @@ gpus = ""  # Leave a blank default
 all_nodes = comm.gather(name, root=0)
 if rank == 0:
     all_nodes = set(all_nodes)
-    if os.getenv("CUDA_HOME"):
+    try:
         import GPUtil as GPU
 
         gpus = "Available GPUs: " + str(GPU.getAvailable(order="first", limit=999))
+    except ImportError:
+        pass
     print("Running %d tasks on nodes %s. %s" % (size, all_nodes, gpus))
     time.sleep(5)
 MPI.Finalize()
