60048dc8466f95cd372b1c11e5fe155172077f72
Alan O'Cais <a.ocais@fz-juelich.de>
2019-05-24 16:15:26 +0200

    Make sure we can run multiple tasks

--- examples/mpi_tasks_srun.py
+++ examples/mpi_tasks_srun.py
@@ -41,12 +41,27 @@ def task1(task_name):
     return return_string
 
 
+@mpi_task(cluster_id="mpiCluster")
+def task2(name, task_name="default"):
+    from mpi4py import MPI
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    # This only appears in the slurm job output
+    return_string = "Hi %s, my rank is %d for task of type %s" % (name, rank, task_name)
+    print(return_string)
+
+    return return_string
+
+
 # @on_cluster()  # LocalCluster
 def main():
     t1 = task1("task1")
     t2 = task1("task1, 2nd iteration")
+    t3 = task2("Alan", task_name="Task 2")
     print(t1.result())
     print(t2.result())
+    print(t3.result())
 
 
 if __name__ == "__main__":
