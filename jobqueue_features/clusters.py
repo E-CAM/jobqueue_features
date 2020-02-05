b82420714931ac6aba1698026ef3448e0709925f
Alan O'Cais <a.ocais@fz-juelich.de>
2020-02-05 11:50:05 +0100

    Revert check

--- jobqueue_features/clusters.py
+++ jobqueue_features/clusters.py
@@ -436,9 +436,6 @@ class CustomClusterMixin(object):
                         "jobqueue.{}.cores".format(self.scheduler_name),
                         default=self.minimum_cores,
                     )
-                    # Value may not be defined to integer
-                    if not isinstance(features_cores, int):
-                        features_cores = self.minimum_cores
             else:
                 features_cores = cores
             if features_cores > self.cores_per_node * self.hyperthreading_factor:
