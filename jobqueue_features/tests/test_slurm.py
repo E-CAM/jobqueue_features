d800fd367691fff28b22d252dbd6367f3295d5e1
Alan O'Cais <a.ocais@fz-juelich.de>
2020-02-14 18:02:35 +0100

    Make example work on CI slurm cluster

--- jobqueue_features/tests/test_slurm.py
+++ jobqueue_features/tests/test_slurm.py
@@ -10,13 +10,6 @@ from jobqueue_features import (
     on_cluster,
     mpi_task,
     which,
-    get_task_mpi_comm,
-    set_task_mpi_comm,
-    serialize_function_and_args,
-    deserialize_and_execute,
-    mpi_deserialize_and_execute,
-    verify_mpi_communicator,
-    flush_and_abort,
     CustomSLURMCluster,
 )
 
@@ -24,7 +17,10 @@ from jobqueue_features import (
 class TestSLURM(TestCase):
     def setUp(self):
         self.number_of_processes = 4
+        # Really hard to get srun in CI, so use mpiexec to keep things simple
+        self.launcher = MPIEXEC
         self.slurm_cluster = CustomSLURMCluster(
+            interface="",
             name="mpiCluster",
             walltime="00:04:00",
             nodes=2,
@@ -35,32 +31,32 @@ class TestSLURM(TestCase):
             memory="256 MB",
             mpi_mode=True,
             fork_mpi=True,
+            env_extra=[
+                "export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1",
+                "export OMPI_ALLOW_RUN_AS_ROOT=1",
+            ],
+            mpi_launcher=self.launcher,
+            local_directory="/tmp",
+            queue="batch",
         )
-
         self.executable = "python"
         self.script_path = os.path.abspath(
             os.path.join(os.path.dirname(__file__), "resources", "helloworld.py")
         )
 
         @on_cluster(cluster=self.slurm_cluster, cluster_id="mpiCluster")
-        @mpi_task(cluster_id="mpiCluster", default_mpi_tasks=4)
+        @mpi_task(cluster_id="mpiCluster")
         def mpi_wrap_task(**kwargs):
             return mpi_wrap(**kwargs)
 
-        @on_cluster(cluster=self.slurm_cluster)
         def test_function(script_path, return_wrapped_command=False):
-            script_path = os.path.join(
-                os.path.dirname(__file__), "resources", "helloworld.py"
-            )
             t = mpi_wrap_task(
                 executable=self.executable,
                 exec_args=script_path,
-                mpi_launcher=SRUN,
-                mpi_tasks=self.number_of_processes,
+                # mpi_tasks=self.number_of_processes,
                 return_wrapped_command=return_wrapped_command,
             )
             result = t.result()
-
             return result
 
         self.test_function = test_function
@@ -70,7 +66,18 @@ class TestSLURM(TestCase):
         #
         # Assume here we have srun support
         if which(SRUN) is not None:
-            print("Found {}, running MPI test".format(SRUN))
+            print(
+                "Found {} so assuming we have Slurm, running MPI test (with {})".format(
+                    SRUN, self.launcher
+                )
+            )
+            launcher = self.launcher
+            # First check we can construct the command
+            result = self.test_function(self.script_path, return_wrapped_command=True)
+            expected_result = "{} -np {} {} {}".format(
+                launcher, self.number_of_processes, self.executable, self.script_path
+            )
+            self.assertEqual(result, expected_result)
             result = self.test_function(self.script_path)
             print(result)
             for n in range(self.number_of_processes):
@@ -78,10 +85,5 @@ class TestSLURM(TestCase):
                     n, self.number_of_processes
                 )
                 self.assertIn(text.encode(), result["out"])
-            result = self.test_function(self.script_path, return_wrapped_command=True)
-            expected_result = "{} {} {} {}".format(
-                SRUN, self.number_of_processes, self.executable, self.script_path
-            )
-            self.assertEqual(result, expected_result)
         else:
             pass
