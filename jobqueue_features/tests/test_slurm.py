34045ddd88fa565fd1b899b018aa5791e364cdc8
Alan O'Cais <a.ocais@fz-juelich.de>
2020-02-20 10:53:07 +0100

    Only run slurm specific tests on slurm docker instances

--- jobqueue_features/tests/test_slurm.py
+++ jobqueue_features/tests/test_slurm.py
@@ -1,7 +1,12 @@
 from __future__ import print_function
+
 from unittest import TestCase
 import os
 import pytest
+
+from jobqueue_features.clusters_controller import (
+    clusters_controller_singleton as controller,
+)
 
 from jobqueue_features import (
     mpi_wrap,
@@ -16,12 +21,15 @@ from jobqueue_features import (
 
 class TestSLURM(TestCase):
     def setUp(self):
+        # Kill any existing clusters
+        controller._close_clusters()
+
         self.number_of_processes = 4
         # Really hard to get srun in CI, so use mpiexec to keep things simple
         self.launcher = MPIEXEC
         self.slurm_cluster = CustomSLURMCluster(
             interface="",
-            name="mpiCluster",
+            name="mpi_cluster",
             walltime="00:04:00",
             nodes=2,
             cores_per_node=2,
@@ -44,8 +52,8 @@ class TestSLURM(TestCase):
             os.path.join(os.path.dirname(__file__), "resources", "helloworld.py")
         )
 
-        @on_cluster(cluster=self.slurm_cluster, cluster_id="mpiCluster")
-        @mpi_task(cluster_id="mpiCluster")
+        @on_cluster(cluster=self.slurm_cluster, cluster_id="mpi_cluster")
+        @mpi_task(cluster_id="mpi_cluster")
         def mpi_wrap_task(**kwargs):
             return mpi_wrap(**kwargs)
 
@@ -56,8 +64,7 @@ class TestSLURM(TestCase):
                 # mpi_tasks=self.number_of_processes,
                 return_wrapped_command=return_wrapped_command,
             )
-            result = t.result()
-            return result
+            return t
 
         self.test_function = test_function
 
@@ -74,11 +81,13 @@ class TestSLURM(TestCase):
             launcher = self.launcher
             # First check we can construct the command
             result = self.test_function(self.script_path, return_wrapped_command=True)
+            result = result.result()
             expected_result = "{} -np {} {} {}".format(
                 launcher, self.number_of_processes, self.executable, self.script_path
             )
             self.assertEqual(result, expected_result)
             result = self.test_function(self.script_path)
+            result = result.result()
             print(result)
             for n in range(self.number_of_processes):
                 text = "Hello, World! I am process {} of {}".format(
