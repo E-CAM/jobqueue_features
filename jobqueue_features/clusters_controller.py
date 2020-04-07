c51994eb9fc8b4b65fe6ad47419584949573e972
AdamWlodarczyk <adam.j.wlodarczyk@gmail.com>
2020-04-07 12:20:12 +0200

    cluster_controller typing refactor

--- jobqueue_features/clusters_controller.py
+++ jobqueue_features/clusters_controller.py
@@ -13,16 +13,14 @@ class ClusterController(object):
     """Controller keeps collection of clusters and clients so it can
     provide these for decorators to submitting tasks."""
 
-    default_cluster = LocalCluster  # type: Callable[..., ClusterType]
+    default_cluster: Callable[..., ClusterType] = LocalCluster
 
     def __init__(self):
-        # type: () -> None
-        self._clusters = {_DEFAULT: None}  # type: Dict[str, ClusterType]
-        self._clients = {_DEFAULT: None}  # type: Dict[str, Client]
+        self._clusters: Dict[str, ClusterType] = {_DEFAULT: None}
+        self._clients: Dict[str, Client] = {_DEFAULT: None}
         atexit.register(self._close)
 
-    def get_cluster(self, id_=None):
-        # type: (str) -> Tuple[ClusterType, Client]
+    def get_cluster(self, id_: str = None) -> Tuple[ClusterType, Client]:
         cluster = self._clusters.get(id_ or _DEFAULT)
         if cluster is None:
             raise ClusterException('No cluster "{}" set!'.format(id_))
@@ -31,8 +29,7 @@ class ClusterController(object):
             raise ClusterException('No client for cluster "{}" set!'.format(id_))
         return cluster, client
 
-    def add_cluster(self, id_=None, cluster=None):
-        # type: (str, ClusterType) -> Tuple[ClusterType, Client]
+    def add_cluster(self, id_: str = None, cluster: ClusterType = None) -> Tuple[ClusterType, Client]:
         if hasattr(cluster, "name"):
             if id_ is None:
                 id_ = cluster.name
@@ -40,20 +37,18 @@ class ClusterController(object):
                 assert id_ == cluster.name
         return self._make_cluster(id_=id_ or _DEFAULT, cluster=cluster)
 
-    def delete_cluster(self, id_):
-        # type: (str) -> None
+    def delete_cluster(self, id_: str) -> None:
         self._close_client(id_=id_)
         self._close_cluster(id_=id_)
 
-    def _make_cluster(self, id_, cluster=None):
-        # type: (str, ClusterType) -> Tuple[ClusterType, Client]
+    def _make_cluster(self, id_: str, cluster: ClusterType = None) -> Tuple[ClusterType, Client]:
         if id_ != _DEFAULT and id_ in self._clusters:
             raise ClusterException('Cluster "{}" already exists!'.format(id_))
         self._clusters[id_] = cluster or self._make_default_cluster(name=id_)
         self._make_client(id_=id_)
         return self._clusters[id_], self._clients[id_]
 
-    def _make_default_cluster(self, name):
+    def _make_default_cluster(self, name: str) -> ClusterType:
         kwargs = {}
         if self.default_cluster is not LocalCluster:
             kwargs["name"] = name
@@ -61,8 +56,7 @@ class ClusterController(object):
             kwargs["processes"] = False
         return self.default_cluster(**kwargs)
 
-    def _make_client(self, id_):
-        # type: (str) -> None
+    def _make_client(self, id_: str) -> None:
         cluster = self._clusters[id_]
         if hasattr(cluster, "client"):
             client = cluster.client
@@ -70,31 +64,26 @@ class ClusterController(object):
             client = Client(cluster)
         self._clients[id_] = client
 
-    def _close(self):
-        # type: () -> None
+    def _close(self) -> None:
         self._close_clients()
         self._close_clusters()
 
-    def _close_cluster(self, id_):
-        # type: (str) -> None
+    def _close_cluster(self, id_: str) -> None:
         cluster = self._clusters.pop(id_, None)
         if cluster and cluster.status != "closed":
             cluster.close()
 
-    def _close_clusters(self):
-        # type: () -> None
+    def _close_clusters(self) -> None:
         for id_ in list(self._clusters.keys()):
             self._close_cluster(id_)
         self._clusters = {_DEFAULT: None}
 
-    def _close_client(self, id_):
-        # type: (str) -> None
+    def _close_client(self, id_: str) -> None:
         client = self._clients.get(id_)
         if client:
             client.close()
 
-    def _close_clients(self):
-        # type: () -> None
+    def _close_clients(self) -> None:
         for id_ in list(self._clients.keys()):
             self._close_client(id_)
         self._clients = {_DEFAULT: None}
