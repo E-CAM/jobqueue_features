3c519eb5dd35356ef9a6c69e7b14be5c4eeb6271
AdamWlodarczyk <adam.j.wlodarczyk@gmail.com>
2021-02-05 02:11:58 +0100

    Ensure to connect to available port (fix: #97)

--- tutorial/jupyter.sh
+++ tutorial/jupyter.sh
@@ -2,9 +2,23 @@
 
 JUPYTER_CONTAINERS_DIR="$(pwd)/$(dirname "${BASH_SOURCE[0]}")"
 
+function assign_available_port() {
+    port=8888
+    while [ `lsof -Pi :$port -sTCP:LISTEN -t` ]
+    do
+      echo "Port $port is already used. Checking $(( $port + 1 ))"
+      port=$(( $port + 1 ))
+    done
+    echo "Port $port is available"
+}
+
+
 function start_slurm() {
+    assign_available_port
     cd "$JUPYTER_CONTAINERS_DIR/docker_config/slurm"
+      echo "SLURMCTLD_OUT_PORT=$port" >> ./.env
       ./start-slurm.sh
+      rm ./.env
     cd -
 
     # Install JupyterLab and the Dask extension
@@ -37,7 +51,7 @@ function launch_tutorial_slurm() {
     docker exec -u slurmuser slurmctld /bin/bash -c "cd /data; git clone https://github.com/E-CAM/jobqueue_features_workshop_materials.git"
     docker exec -u slurmuser slurmctld /bin/bash -c "jupyter lab workspace import /data/jobqueue_features_workshop_materials/workspace.json"
     docker exec -u slurmuser slurmctld /bin/bash -c "cd /data/jobqueue_features_workshop_materials; jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.notebook_dir='/data/jobqueue_features_workshop_materials'&"
-    echo -e "\tOpen your browser at http://localhost:8888/lab/workspaces/lab"
+    echo -e "\tOpen your browser at http://localhost:$port/lab/workspaces/lab"
     echo
 }
 
@@ -46,7 +60,7 @@ function start_jobqueue_tutorial() {
     # Clone jobqueue tutorial, import workspace
     docker exec -u slurmuser slurmctld /bin/bash -c "cd /data; git clone https://github.com/ExaESM-WP4/workshop-Dask-Jobqueue-cecam-2021-02.git"
     docker exec -u slurmuser slurmctld /bin/bash -c "cd /data/workshop-Dask-Jobqueue-cecam-2021-02; jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.notebook_dir='/data/workshop-Dask-Jobqueue-cecam-2021-02'&"
-    echo -e "\tOpen your browser at http://localhost:8888/lab/workspaces/lab"
+    echo -e "\tOpen your browser at http://localhost:$port/lab/workspaces/lab"
     echo
 }
 
